79a9917536280dab323024595b7e62e49cb7d6de
Sergey Ivanov <plastilin1998@gmail.com>
2020-05-18 14:30:46 +0800

    Добавление Refresh для показаний

--- xamarinJKH/Main/CountersPage.xaml.cs
+++ xamarinJKH/Main/CountersPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using xamarinJKH.Server;
@@ -17,6 +17,38 @@ namespace xamarinJKH.Main
     {
         public List<MeterInfo> _meterInfo { get; set; }
         private RestClientMP _server = new RestClientMP();
+        private bool _isRefreshing = false;
+        
+        public bool IsRefreshing
+        {
+            get { return _isRefreshing; }
+            set {
+                _isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    IsRefreshing = true;
+                    await RefreshData();
+                    IsRefreshing = false;
+                });
+            }
+        }
+
+        private async Task RefreshData()
+        {
+            ItemsList<MeterInfo> info = await _server.GetThreeMeters();
+            _meterInfo = info.Data;
+            countersList.ItemsSource = null;
+            countersList.ItemsSource = _meterInfo;
+        }
+
         public CountersPage()
         {
             InitializeComponent();
