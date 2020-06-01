90a8a9e05d1e55286baa76427222eee35e5eefc8
ipon85new <ipon85@yandex.ru>
2020-06-02 00:34:36 +0300

    на странице RegisterForm в селекторе даты рождения DateTimePicker заменен на кастомный, с целью убрать подчеркивание даты для андроида и контур для ios

---
+++ xamarinJKH.iOS/CustomRenderers/BorderlessDatePickerRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using xamarinJKH;
+using xamarinJKH.iOS.CustomRenderers;
+
+[assembly: ExportRenderer(typeof(BorderlessDatePicker), typeof(BorderlessDatePickerRenderer))]
+namespace xamarinJKH.iOS.CustomRenderers
+{
+    public class BorderlessDatePickerRenderer : DatePickerRenderer
+    {
+        public static void Init() { }
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
