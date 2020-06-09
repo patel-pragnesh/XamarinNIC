17b511106ffa88fffbb88b60f6df842f0bd59891
evgen <koles.doc96.09.27@gmail.com>
2020-06-09 11:40:19 +0700

    Тост

---
+++ xamarinJKH/DialogViews/ToastDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AiForms.Dialogs.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarinJKH.DialogViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToastDialog : ToastView
+    {
+        public ToastDialog()
+        {
+            InitializeComponent();
+        }
+        
+        // define appearing animation
+        public override void RunPresentationAnimation() {}
+
+        // define disappearing animation
+        public override void RunDismissalAnimation() {}
+
+        // define clean up process.
+        public override void Destroy() {}
+    }
+}
