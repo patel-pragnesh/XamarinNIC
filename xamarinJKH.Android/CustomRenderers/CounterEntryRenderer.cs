619f53b6b49a52c572b61dc491b431237f12e299
Anton Motin <anton.motin.dev@gmail.com>
2020-06-09 19:44:58 +0300

    Свой entry для ввода показателей и лучшей логики ввода в будущем

---
+++ xamarinJKH.Android/CustomRenderers/CounterEntryRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using xamarinJKH.CustomRenderers;
+using xamarinJKH.Droid.CustomRenderers;
+
+using System.ComponentModel;
+
+//[assembly: ExportRenderer(typeof(CounterEntry), typeof(CounterEntryRenderer))]
+namespace xamarinJKH.Droid.CustomRenderers
+{
+    public class CounterEntryRenderer : EntryRenderer
+    {
+        public CounterEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == "Text")
+            {
+                var charachter = Control.Text;
+            }
+
+        }
+    }
+}
---
+++ xamarinJKH/CustomRenderers/CounterEntry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xamarin.Forms;
+
+namespace xamarinJKH.CustomRenderers
+{
+    public class CounterEntry:Entry
+    {
+        //public static BindableProperty CharachterCount = BindableProperty.Create("CharCount", typeof(string), typeof(string), 9);
+        //public string CharCount
+        //{
+        //    set => SetValue(CharachterCount, value);
+        //}
+
+        bool DecimalPart;
+        public CounterEntry() : base()
+        {
+            //var characters = int.Parse((string)GetValue(CharachterCount));
+            //this.CharacterSpacing = this.Width / characters * 2;
+            this.CursorPosition = 5;
+            this.Focus();
+            this.TextChanged += (sender, e) =>
+            {
+                //try
+                //{
+                //    if (e.NewTextValue == "00000.000")
+                //    {
+                //        this.Text = string.Empty;
+                //        return;
+                //    }
+                //    var newval = 0.0; double.TryParse(e.NewTextValue.Replace(',', '.'), out newval);
+                //    var oldval = 0.0; double.TryParse(e.OldTextValue.Replace(',', '.'), out oldval);
+                //    this.Text = String.Format("{0:00000.000}", newval);
+                //    var Decimal = newval - Math.Floor(newval) > 0;
+                //    if (!Decimal)
+                //        this.CursorPosition = 5;
+
+                //    if (e.NewTextValue.Length < e.OldTextValue.Length)
+                //    {
+                //        this.CursorPosition = e.NewTextValue.Length - 1;
+                //    }
+                    
+                    
+                //}
+                //catch
+                //{
+
+                //}
+            };
+        }
+    }
+}
