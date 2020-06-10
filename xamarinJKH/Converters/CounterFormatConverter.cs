e236bddb73e2307c3592c19a5e6d97acdafe564e
Anton Motin <anton.motin.dev@gmail.com>
2020-06-10 15:20:28 +0300

    Конвертер для форматирования строки показаний счетчика

---
+++ xamarinJKH/Converters/CounterFormatConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace xamarinJKH.Converters
+{
+    public class CounterFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = decimal.Parse(value.ToString());
+            return String.Format("{0:00000.000}", val).Replace('.',',');
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
