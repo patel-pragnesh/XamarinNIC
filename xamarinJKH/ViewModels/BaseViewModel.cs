0cae630e582e663a0e6c1788e5a0f0b462ccb173
Anton Motin <anton.motin.dev@gmail.com>
2020-06-10 15:23:36 +0300

    Базовая вьюмодель добавлена

---
+++ xamarinJKH/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace xamarinJKH.ViewModels
+{
+    public class BaseViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
