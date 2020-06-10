8da73e11479007a6d25ee6dde8af9f3ef8bbf1a2
Anton Motin <anton.motin.dev@gmail.com>
2020-06-10 15:20:56 +0300

    Добавлена вьюмодель для страницы добавления значений счетчиков

---
+++ xamarinJKH/Counters/AddMetersPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using xamarinJKH.ViewModels;
+using Xamarin.Forms;
+
+namespace xamarinJKH.Counters
+{
+    public class AddMetersPageViewModel:BaseViewModel
+    {
+        decimal _prevCount;
+        public decimal PrevCounter
+        {
+            get => _prevCount;
+            set
+            {
+                _prevCount = value;
+                OnPropertyChanged("PrevCount");
+            }
+        }
+
+        public AddMetersPageViewModel(decimal value)
+        {
+            PrevCounter = value;
+        }
+    }
+}
