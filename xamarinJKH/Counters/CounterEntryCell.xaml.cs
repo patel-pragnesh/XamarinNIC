30f021e2473677522438a565ea095f0a1e761304
Anton Motin <anton.motin.dev@gmail.com>
2020-06-10 15:23:19 +0300

    Добавлена проперти, с помощью которой устанавливаются нужные скругления, цвет подчеркивания берется из настроек

--- xamarinJKH/Counters/CounterEntryCell.xaml.cs
+++ xamarinJKH/Counters/CounterEntryCell.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,40 @@ namespace xamarinJKH.Counters
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CounterEntryCell : ContentView
     {
+        public static BindableProperty Index = BindableProperty.Create("CellIndex", typeof(string), typeof(string), null);
+        public string CellIndex
+        {
+            set
+            {
+                SetValue(Index, value);
+                SetCorners(int.Parse(value));
+            }
+            get => (string)GetValue(Index);
+        }
         public CounterEntryCell()
         {
             InitializeComponent();
+            BindingContext = new CounterCellViewModel();
+            
+        }
+
+        void SetCorners(int index)
+        {
+            if (index == 0)
+            {
+                Pancake.CornerRadius = new CornerRadius(5, 5, 5, 0);
+            }
+
+            if (index == 8)
+            {
+                Pancake.CornerRadius = new CornerRadius(5, 5, 0, 5);
+            }
+        }
+
+        internal class CounterCellViewModel
+        {
+            public Color CellColor => Color.FromHex(xamarinJKH.Utils.Settings.MobileSettings.color);
         }
     }
+
 }
