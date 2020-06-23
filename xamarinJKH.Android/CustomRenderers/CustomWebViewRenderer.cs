6389f024658878e65c8a7f292ea7c1d775becfa2
Anton Motin <anton.motin.dev@gmail.com>
2020-06-23 13:32:43 +0300

    Кастомный веб-вью для просмотра документов для андроида

---
+++ xamarinJKH.Android/CustomRenderers/CustomWebViewRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using xamarinJKH.Droid.CustomRenderers;
+using xamarinJKH.CustomRenderers;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using System.Net;
+
+[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace xamarinJKH.Droid.CustomRenderers
+{
+    public class CustomWebViewRenderer : WebViewRenderer
+    {
+        public CustomWebViewRenderer(Context context) : base(context) { }
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var customWebView = Element as CustomWebView;
+                Control.Settings.AllowUniversalAccessFromFileURLs = true;
+                Control.Settings.JavaScriptEnabled = true;
+                Control.Settings.JavaScriptCanOpenWindowsAutomatically = true;
+                Control.LoadUrl(String.Format("https://docs.google.com/viewer?url={0}", customWebView.Uri));
+                //Control.LoadUrl(string.Format("file:///android_asset/pdfjs/web/viewer.html?file={0}", string.Format("file:///{0}", customWebView.Uri)));
+            }
+        }
+    }
+}
---
+++ xamarinJKH/CustomRenderers/CustomWebView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace xamarinJKH.CustomRenderers
+{
+    public class CustomWebView:WebView
+    {
+        public static BindableProperty URIProperty = BindableProperty.Create("Uri", typeof(string), typeof(CustomWebView), default(string), BindingMode.TwoWay);
+        public string Uri
+        {
+            get => (string)GetValue(URIProperty);
+            set => SetValue(URIProperty, value);
+        }
+    }
+}
