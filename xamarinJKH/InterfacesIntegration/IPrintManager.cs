479a89ada29711962c6576ce4ebba5ac202b5342
Anton Motin <anton.motin.dev@gmail.com>
2020-06-24 12:18:50 +0300

    Интерфейс для печати

---
+++ xamarinJKH/InterfacesIntegration/IPrintManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xamarinJKH.InterfacesIntegration
+{
+    public interface IPrintManager
+    {
+        void SendFileToPrint(byte[] file);
+    }
+}
