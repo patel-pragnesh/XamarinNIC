804649bdb2334f12f3b44063cbf59cf7cadad318
evgen <koles.doc96.09.27@gmail.com>
2020-06-29 09:06:13 +0700

    Апи для мониторинга

---
+++ xamarinJKH/Server/RequestModel/Monitor/PeriodStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace xamarinJKH.Server.RequestModel
+{
+    public class PeriodStats
+    {
+        public int RequestsCount { get; set; }
+        public List<Requests> UnperformedRequestsList { get; set; }
+        public List<Requests> OverdueRequestsList { get; set; }
+        public int ClosedRequestsWithMark1Count { get; set; }
+        public List<Requests> ClosedRequestsWithMark1List { get; set; }
+        public int ClosedRequestsWithMark2Count { get; set; }
+        public List<Requests> ClosedRequestsWithMark2List { get; set; }
+        public int ClosedRequestsWithMark3Count { get; set; }
+        public List<Requests> ClosedRequestsWithMark3List { get; set; }
+        public int ClosedRequestsWithMark4Count { get; set; }
+        public List<Requests> ClosedRequestsWithMark4List { get; set; }
+        public int ClosedRequestsWithMark5Count { get; set; }
+        public List<Requests> ClosedRequestsWithMark5List { get; set; }
+    }
+}
