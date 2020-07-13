e4c6edaba9ce96ee5b46ba57bd33ea103b72336c
Anton Motin <anton.motin.dev@gmail.com>
2020-07-13 16:52:59 +0300

    Интерфейс для получения токена firebase в общем проекте

---
+++ xamarinJKH/InterfacesIntegration/IFirebaseTokenObtainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarinJKH.InterfacesIntegration
+{
+    public interface IFirebaseTokenObtainer
+    {
+        public string GetToken();
+    }
+}
