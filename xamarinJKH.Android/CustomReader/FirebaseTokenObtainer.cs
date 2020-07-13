2decd9987ce4d2950e2ceefbe05c3a0ec1c549a5
Anton Motin <anton.motin.dev@gmail.com>
2020-07-13 16:51:34 +0300

    Реализация получения токена девайса

---
+++ xamarinJKH.Android/CustomReader/FirebaseTokenObtainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using xamarinJKH.InterfacesIntegration;
+using Xamarin.Forms;
+using xamarinJKH.Droid.CustomReader;
+
+[assembly:Dependency(typeof(FirebaseTokenObtainer))]
+namespace xamarinJKH.Droid.CustomReader
+{
+    public class FirebaseTokenObtainer : IFirebaseTokenObtainer
+    {
+        public string GetToken()
+        {
+            return Firebase.Iid.FirebaseInstanceId.Instance.Token;
+        }
+    }
+}
