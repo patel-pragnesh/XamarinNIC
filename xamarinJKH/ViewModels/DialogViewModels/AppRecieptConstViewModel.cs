05baa5aba9c5b6776e39c53b0d88b074df0db30d
Anton Motin <anton.motin.dev@gmail.com>
2020-08-14 18:27:06 +0300

    Обработка ввода при редактировании, команда на обновление чека

--- xamarinJKH/ViewModels/DialogViewModels/AppRecieptConstViewModel.cs
+++ xamarinJKH/ViewModels/DialogViewModels/AppRecieptConstViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using Xamarin.Forms;
 using xamarinJKH.Server.RequestModel;
@@ -12,6 +13,7 @@ namespace xamarinJKH.ViewModels.DialogViewModels
         public ObservableCollection<RequestsReceiptItem> ReceiptItems { get; set; }
         public Command Delete { get; set; }
         public Command Edit { get; set; }
+        public Command Update { get; set; }
         public int ID { get; set; }
 
         public AppRecieptConstViewModel(List<RequestsReceiptItem> items, int id)
@@ -34,9 +36,27 @@ namespace xamarinJKH.ViewModels.DialogViewModels
 
             Edit = new Command<RequestsReceiptItem>(item => 
             {
-                if (item != null)
+            });
+
+            Update = new Command(async () =>
+            {
+                var response = await Server.UpdateReceipt(new RequestsReceiptItemsList(this.ID, ReceiptItems));
+                
+            });
+
+            MessagingCenter.Subscribe<Object, List<Goods>>(this, "AddItems", (sender, args) =>
+            {
+                if (args != null)
                 {
-                    MessagingCenter.Send<AppRecieptConstViewModel, string>(this, "SelectPrice", item.Name);
+                    foreach (var item in args)
+                    {
+                        var receipt_item = new RequestsReceiptItem { Name = item.Name, Price = item.Price, Amount = 1 };
+                        var existing = ReceiptItems.First(x => x.Name == item.Name);
+                        if (existing == null)
+                            Device.BeginInvokeOnMainThread(() => ReceiptItems.Add(receipt_item));
+                        else
+                            existing.Amount++;
+                    }
                 }
             });
         }
