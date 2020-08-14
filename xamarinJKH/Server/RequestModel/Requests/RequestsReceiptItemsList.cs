852b99af468a96bcca329b46bec5d885a065d7d7
Anton Motin <anton.motin.dev@gmail.com>
2020-08-14 18:27:33 +0300

    Изменения в модели, добавление модели для обновления

---
+++ xamarinJKH/Server/RequestModel/Requests/RequestsReceiptItemsList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarinJKH.Server.RequestModel
+{
+    public class RequestsReceiptItemsList
+    {
+        public int RequestId { get; set; }
+        public List<RequestsReceiptItem> ReceiptItems { get; set; }
+
+        public RequestsReceiptItemsList(int id, IEnumerable<RequestsReceiptItem> items)
+        {
+            RequestId = id;
+            ReceiptItems = new List<RequestsReceiptItem>();
+            ReceiptItems.AddRange(items);
+        }
+    }
+}
