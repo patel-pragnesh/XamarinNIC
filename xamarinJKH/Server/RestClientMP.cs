dffd4073e3b3724587550bcd941d1bea0a36d830
ipon85new <ipon85@yandex.ru>
2020-08-17 19:36:47 +0300

    -  в комментариях к заявке у "Сотрудника" исправлено отображение строки ввода комментария при вводе текста

--- xamarinJKH/Server/RestClientMP.cs
+++ xamarinJKH/Server/RestClientMP.cs
@@ -16,7 +16,7 @@ namespace xamarinJKH.Server
     public class RestClientMP
     {
         // public const string SERVER_ADDR = "https://api.sm-center.ru/test_erc_udm"; // ОСС
-        public const string SERVER_ADDR = "https://api.sm-center.ru/komfortnew"; // Гранель
+        //public const string SERVER_ADDR = "https://api.sm-center.ru/komfortnew"; // Гранель
         //public const string SERVER_ADDR = "https://api.sm-center.ru/water"; // Тихая гавань
         // public const string SERVER_ADDR = "https://api.sm-center.ru/dgservicnew"; // Домжил (дом24)
         // public const string SERVER_ADDR = "https://api.sm-center.ru/UKUpravdom"; //Управдом Чебоксары
@@ -29,7 +29,7 @@ namespace xamarinJKH.Server
         //public const string SERVER_ADDR = "https://api.sm-center.ru/ukom"; // УК Огни Москвы
         //public const string SERVER_ADDR = "https://api.sm-center.ru/tsg_svyato-troitskii15"; // УК Свято троицк
         //public const string SERVER_ADDR = "https://api.sm-center.ru/ooo_uk_rks_aud"; // УК РКС
-        //public const string SERVER_ADDR = "https://api.sm-center.ru/profikomfort"; // Профи комфорт
+        public const string SERVER_ADDR = "https://api.sm-center.ru/profikomfort"; // Профи комфорт
 
         public const string SEND_TEACH_MAIL = "Public/TechSupportAppeal"; // Создание обращения в тех поддержк
         public const string LOGIN_DISPATCHER = "auth/loginDispatcher"; // Аутентификация сотрудника
