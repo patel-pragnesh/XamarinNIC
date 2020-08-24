7925f8570562331e6e312358a7d27ff0ea3ec97a
Anton Motin <anton.motin.dev@gmail.com>
2020-08-24 17:36:01 +0300

    Обработка скролла в странице магазина

--- xamarinJKH/Shop/ShopPageNew.xaml.cs
+++ xamarinJKH/Shop/ShopPageNew.xaml.cs
@@ -25,5 +25,27 @@ namespace xamarinJKH.Shop
         {
             await Navigation.PopAsync();
         }
+
+        private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            var index = e.FirstVisibleItemIndex;
+            var delta = e.HorizontalDelta;
+            if (index >= 0)
+            {
+                if (index + 1 <= viewModel.Categories.Count - 1 && e.HorizontalOffset < 160)
+                {
+                    viewModel.SelectedCategory = viewModel.Categories[index];
+                }
+                else
+                {
+                    viewModel.SelectedCategory = viewModel.Categories[e.LastVisibleItemIndex];
+                }
+            }
+            if (viewModel.SelectedCategory == " ")
+            {
+                var id = viewModel.Categories.ToList().IndexOf(" ");
+                (sender as CollectionView).ScrollTo(id - 1);// viewModel.Categories[id - 1];
+            }
+        }
     }
 }
