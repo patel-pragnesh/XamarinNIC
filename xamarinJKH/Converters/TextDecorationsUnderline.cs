4697cf3164642278ded09e0f21381b2f82e99a54
Anton Motin <anton.motin.dev@gmail.com>
2020-08-24 17:34:47 +0300

    Костыль для конвертации подчеркивания в лейбле

---
+++ xamarinJKH/Converters/TextDecorationsUnderline.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace xamarinJKH.Converters
+{
+    public class TextDecorationsUnderline : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color color = (Color)value;
+            if (color != Color.Black || color != Color.White)
+            {
+                return TextDecorations.Underline;
+            }
+            else
+            {
+                return TextDecorations.None;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return TextDecorations.None;
+        }
+    }
+}
