ca837b81f0df5d1c1c95a0a5af73bd727b12e669
Anton Motin <anton.motin.dev@gmail.com>
2020-08-31 17:55:19 +0300

    Тестовая страница с видеоплеером

---
+++ xamarinJKH/VideoStreaming/TestVideoPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarinJKH.VideoStreaming
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestVideoPage : ContentPage
+    {
+        public string TestUrl
+        {
+            get => "https://vs.domru.ru/translation?id=331972335&guid=dc3b372c7e421f19192a&mode=hls";
+        }
+        public TestVideoPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
