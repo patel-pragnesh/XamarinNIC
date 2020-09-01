74f562fe062b3c1ffa9f4dd77c7adff8b26f1a4b
Anton Motin <anton.motin.dev@gmail.com>
2020-09-01 18:03:34 +0300

    Страницы для просмотра камеры

---
+++ xamarinJKH/VideoStreaming/CameraPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarinJKH.VideoStreaming
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraPage : ContentPage
+    {
+        string _link;
+        public string Link
+        {
+            get => _link;
+            set
+            {
+                _link = value;
+                OnPropertyChanged("Link");
+            }
+        }
+        public CameraPage(string link)
+        {
+            InitializeComponent();
+            BindingContext = this;
+            Link = link;
+        }
+    }
+}
