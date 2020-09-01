1eaeb92041e8304f0c0287eb4bef92582dd9dee1
Anton Motin <anton.motin.dev@gmail.com>
2020-09-01 18:04:04 +0300

    Еще один тип шапки страницы

---
+++ xamarinJKH/HeaderViewStack.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Rg.Plugins.Popup.Services;
+using xamarinJKH.DialogViews;
+
+namespace xamarinJKH
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HeaderViewStack : ContentView
+    {
+        public static BindableProperty TitleProperty = BindableProperty.Create("Title", typeof(string), typeof(string));
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+        public HeaderViewStack()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        async void Tech(object sender, EventArgs args)
+        {
+            await PopupNavigation.Instance.PushAsync(new TechDialog());
+        }
+
+        void Back(object sender, EventArgs args)
+        {
+            MessagingCenter.Send<HeaderViewStack>(this, "GoBack");
+        }
+    }
+}
