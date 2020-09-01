eb9a950897998f683118bc28b5d0d2a4a50a07eb
Anton Motin <anton.motin.dev@gmail.com>
2020-09-01 18:03:19 +0300

    Страница списка камер, пока нет загрузки данных с сервера

---
+++ xamarinJKH/ViewModels/VideoStreamingViewModels/CameraListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+using Xamarin.Forms;
+using xamarinJKH.Server.RequestModel;
+
+namespace xamarinJKH.ViewModels.VideoStreamingViewModels
+{
+    public class CameraListViewModel:BaseViewModel
+    {
+        public ObservableCollection<CameraModel> Cameras { get; set; }
+        public CameraListViewModel()
+        {
+            Cameras = new ObservableCollection<CameraModel>();
+            Cameras.Add(new CameraModel() { Name = "Test camera", Link = "https://vs.domru.ru/translation?id=331972335&guid=dc3b372c7e421f19192a&mode=hls" });
+        }
+    }
+}
