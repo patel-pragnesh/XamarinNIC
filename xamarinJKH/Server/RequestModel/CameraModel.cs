495def77cdff64e81938ebf38f7c46f5c79c2c64
Anton Motin <anton.motin.dev@gmail.com>
2020-09-01 18:02:54 +0300

    Начальная модель для запроса камер

---
+++ xamarinJKH/Server/RequestModel/CameraModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarinJKH.Server.RequestModel
+{
+    public class CameraModel
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
