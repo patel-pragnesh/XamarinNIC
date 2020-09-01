f362801062bf0317064e254d95504f801ddb7e75
Anton Motin <anton.motin.dev@gmail.com>
2020-09-01 18:03:19 +0300

    Страница списка камер, пока нет загрузки данных с сервера

---
+++ xamarinJKH/VideoStreaming/CameraListPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using xamarinJKH.Server.RequestModel;
+using xamarinJKH.ViewModels.VideoStreamingViewModels;
+
+namespace xamarinJKH.VideoStreaming
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraListPage : ContentPage
+    {
+        public CameraListPage()
+        {
+            InitializeComponent();
+            BindingContext = new CameraListViewModel();
+
+            MessagingCenter.Subscribe<HeaderViewStack>(this, "GoBack", sender =>
+            {
+                Navigation.PopAsync();
+            });
+        }
+
+        async void CameraSelect(object sender, SelectionChangedEventArgs args)
+        {
+            try
+            {
+                var camera = args.CurrentSelection[0] as CameraModel;
+                await Navigation.PushAsync(new CameraPage(camera.Link));
+                (sender as CollectionView).SelectedItem = null;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
