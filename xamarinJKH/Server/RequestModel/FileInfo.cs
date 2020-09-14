6566225df3595546849f347648efb2df9e35b494
evgen <koles.doc96.09.27@gmail.com>
2020-09-14 14:45:05 +0700

    - Файлы в уведомлениях

---
+++ xamarinJKH/Server/RequestModel/FileInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace xamarinJKH.Server.RequestModel
+{
+    public class FileInfo
+    {
+        // описание
+        public string Description { get; set; }
+        // cсылка на файл
+        public string Link { get; set; }
+        // имя файла
+        public string Name { get; set; }
+    }
+}
