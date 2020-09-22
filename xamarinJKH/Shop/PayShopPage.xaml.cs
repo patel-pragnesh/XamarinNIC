51f0506041b00adcdafda6fecd8abd2844b5a2c3
evgen <koles.doc96.09.27@gmail.com>
2020-09-22 14:42:57 +0700

    - Правка расскраски иконок в магазине - Правка подсчета списания бонусов

--- xamarinJKH/Shop/PayShopPage.xaml.cs
+++ xamarinJKH/Shop/PayShopPage.xaml.cs
@@ -136,7 +136,8 @@ namespace xamarinJKH.Shop
                         }
                         else
                         {
-                            bonus = 0;
+                            bonus = bonusCount;
+                            bonusCount = 0;
                         }
                     }
 
