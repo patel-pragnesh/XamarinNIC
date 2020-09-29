bd919778b10cf884cd3bfe182592de3591e6ae49
Anton Motin <anton.motin.dev@gmail.com>
2020-09-29 13:51:13 +0300

    Новые фразы в ресурсах

--- xamarinJKH/AppResources.Designer.cs
+++ xamarinJKH/AppResources.Designer.cs
@@ -19,7 +19,7 @@ namespace xamarinJKH {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class AppResources {
@@ -372,6 +372,15 @@ namespace xamarinJKH {
         internal static string Automatic {
             get {
                 return ResourceManager.GetString("Automatic", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Авто показания.
+        /// </summary>
+        internal static string AutoPennance {
+            get {
+                return ResourceManager.GetString("AutoPennance", resourceCulture);
             }
         }
         
