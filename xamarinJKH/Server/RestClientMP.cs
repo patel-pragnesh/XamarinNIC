3dcd75b97ad4c2c1308fcdcc28bf5de2d7a67203
Ilya Ponomarev <ipon85@yandex.com>
2020-09-30 18:21:41 +0300

    - УК Огни Москвы для аппцентра

--- xamarinJKH/Server/RestClientMP.cs
+++ xamarinJKH/Server/RestClientMP.cs
@@ -18,7 +18,7 @@ namespace xamarinJKH.Server
     {
         // public const string SERVER_ADDR = "https://api.sm-center.ru/test_erc_udm"; // ОСС
         // public const string SERVER_ADDR = "https://api.sm-center.ru/komfortnew"; // Гранель
-        public const string SERVER_ADDR = "https://api.sm-center.ru/water"; // Тихая гавань water/ water2 - тихая гавань - 2 
+        //public const string SERVER_ADDR = "https://api.sm-center.ru/water"; // Тихая гавань water/ water2 - тихая гавань - 2 
         // public const string SERVER_ADDR = "https://api.sm-center.ru/dgservicnew"; // Домжил (дом24)
         // public const string SERVER_ADDR = "https://api.sm-center.ru/UKUpravdom"; //Управдом Чебоксары
         // public const string SERVER_ADDR = "https://api.sm-center.ru/uk_sibir_alians"; //Альянс
@@ -29,7 +29,7 @@ namespace xamarinJKH.Server
         // public const string SERVER_ADDR = "https://api.sm-center.ru/ooo_zip"; // ЗИП
         //public const string SERVER_ADDR = "https://api.sm-center.ru/ukom"; // УК Огни Москвы
         //public const string SERVER_ADDR = "https://api.sm-center.ru/tsg_svyato-troitskii15"; // УК Свято троицк
-        //public const string SERVER_ADDR = "https://api.sm-center.ru/ooo_uk_rks"; // УК РКС
+        public const string SERVER_ADDR = "https://api.sm-center.ru/ooo_uk_rks"; // УК РКС
         // public const string SERVER_ADDR = "https://api.sm-center.ru/profikomfort"; // Профи комфорт
         //public const string SERVER_ADDR = "https://api.sm-center.ru/tafgai"; // Тафгай
         //public const string SERVER_ADDR = "https://api.sm-center.ru/eco_lk"; // Авалон эко
