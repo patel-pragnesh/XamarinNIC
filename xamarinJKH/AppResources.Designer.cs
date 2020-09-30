3ec56c931a651ed43e0ffad328e69c8336bedc66
evgen <koles.doc96.09.27@gmail.com>
2020-09-30 15:06:08 +0700

    -Массовое закрытие заявок

--- xamarinJKH/AppResources.Designer.cs
+++ xamarinJKH/AppResources.Designer.cs
@@ -19,7 +19,7 @@ namespace xamarinJKH {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class AppResources {
@@ -300,6 +300,24 @@ namespace xamarinJKH {
         internal static string AppErrorFill {
             get {
                 return ResourceManager.GetString("AppErrorFill", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Заявки закрыты.
+        /// </summary>
+        internal static string AppsClosed {
+            get {
+                return ResourceManager.GetString("AppsClosed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Заявки выполнены.
+        /// </summary>
+        internal static string AppsPerformed {
+            get {
+                return ResourceManager.GetString("AppsPerformed", resourceCulture);
             }
         }
         
