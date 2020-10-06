470d1054dcfb138074dff8d064520f9f8d5a7ed0
Ilya Ponomarev <ipon85@yandex.com>
2020-10-07 00:14:39 +0300

    - файлы для КТК GAlaxy

--- xamarinJKH/Server/RestClientMP.cs
+++ xamarinJKH/Server/RestClientMP.cs
@@ -18,7 +18,7 @@ namespace xamarinJKH.Server
     {
         // public const string SERVER_ADDR = "https://api.sm-center.ru/test_erc_udm"; // ОСС
         // public const string SERVER_ADDR = "https://api.sm-center.ru/komfortnew"; // Гранель
-        public const string SERVER_ADDR = "https://api.sm-center.ru/water"; // Тихая гавань water/ water2 - тихая гавань - 2 
+        //public const string SERVER_ADDR = "https://api.sm-center.ru/water"; // Тихая гавань water/ water2 - тихая гавань - 2 
         // public const string SERVER_ADDR = "https://api.sm-center.ru/dgservicnew"; // Домжил (дом24)
         // public const string SERVER_ADDR = "https://api.sm-center.ru/UKUpravdom"; //Управдом Чебоксары
         // public const string SERVER_ADDR = "https://api.sm-center.ru/uk_sibir_alians"; //Альянс
@@ -53,8 +53,9 @@ namespace xamarinJKH.Server
         //public const string SERVER_ADDR = "https://api.sm-center.ru/vestsnab_xml/"; // ВестСнаб
         //public const string SERVER_ADDR = "https://api.sm-center.ru/tsg_saburova/"; // Сабурово
         //public const string SERVER_ADDR = "https://api.sm-center.ru/tsg_krasnoarmeiskii12/"; // ТСЖ Красноармейская 12
+        public const string SERVER_ADDR = "https://api.sm-center.ru/ooo_lider_ulan_ude"; // КТК Galaxy
 
-        
+
 
         public const string SEND_TEACH_MAIL = "Public/TechSupportAppeal"; // Создание обращения в тех поддержк
         public const string LOGIN_DISPATCHER = "auth/loginDispatcher"; // Аутентификация сотрудника
