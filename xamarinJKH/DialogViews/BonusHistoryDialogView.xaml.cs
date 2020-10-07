97790ff28e6420b63f96cbccc03fe8fce3abe2d6
Anton Motin <anton.motin.dev@gmail.com>
2020-10-07 12:05:14 +0300

    Добавлено окно просмотра истории

---
+++ xamarinJKH/DialogViews/BonusHistoryDialogView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarinJKH.DialogViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BonusHistoryDialogView : ContentPage
+    {
+        public BonusHistoryDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
