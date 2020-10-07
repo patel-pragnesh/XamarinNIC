8b843e75301cab674199dc849bed092293930e1d
Anton Motin <anton.motin.dev@gmail.com>
2020-10-07 12:06:12 +0300

    VM для окна истории бонусов

---
+++ xamarinJKH/ViewModels/DialogViewModels/BonusHistoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+using xamarinJKH.Server.RequestModel;
+
+namespace xamarinJKH.ViewModels.DialogViewModels
+{
+    public class BonusHistoryViewModel:BaseViewModel
+    {
+        public ObservableCollection<BonusCashFlow> History { get; set; }
+        public Command LoadHistory { get; set; }
+    }
+}
