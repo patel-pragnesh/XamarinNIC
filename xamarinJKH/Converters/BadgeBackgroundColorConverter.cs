f4950fc83b58686441cdf3e834ccacd5a18ed5bf
alzaburetz <anton.motin.dev@gmail.com>
2020-10-20 13:56:45 +0300

    Конвертер для бейджа (прозрачный когда количество = 0, красный, если больше)

---
+++ xamarinJKH/Converters/BadgeBackgroundColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace xamarinJKH.Converters
+{
+    public class BadgeBackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int val = (int)value;
+            return val > 0 ? Color.Red : Color.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
