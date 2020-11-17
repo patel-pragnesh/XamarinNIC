fb6d51a5b4b7cf746c381c735534806099360cf1
Anton Motin <anton.motin.dev@gmail.com>
2020-11-17 15:44:25 +0300

    Добавлен плагин для ChromeTabs (не работает)

---
+++ xamarinJKH.Android/CustomRenderers/ChromeTabRenderer.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Net;
+
+using Xamarin.Android.ChromeCustomTabs;
+using Android.Support.CustomTabs.Chromium.SharedUtilities;
+using Android.Support.CustomTabs;
+using Android.Support.CustomTabs.Shared;
+using Xamarin.Forms.Platform.Android;
+using Android.Webkit;
+
+using xamarinJKH.InterfacesIntegration;
+using xamarinJKH.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Plugin.CurrentActivity;
+
+using Org.Chromium.Chrome;
+using Org.Chromium.Chrome.Browser.Hosted;
+using Org;
+using Org.Chromium;
+using Org.Chromium.Chrome.Browser;
+
+[assembly:Dependency(typeof(ChromeTabRenderer))]
+namespace xamarinJKH.Droid.CustomRenderers
+{
+    public class ChromeTabRenderer:IChromeTabs
+    {
+        Activity activity { get; set; }
+        public ChromeTabRenderer()
+        {
+            activity = CrossCurrentActivity.Current.Activity;
+        }
+        public void OpenTab(string uri)
+        {
+            var uiBuilder = new HostedUIBuilder();
+            
+            var manager = (CrossCurrentActivity.Current.Activity as MainActivity).hostedManager;
+            manager.BindService();
+            manager.LoadUrl("https://google.com/", uiBuilder);
+
+            var url = "https://www.xamarin.com";
+            var customTabsIntent = new CustomTabsIntent.Builder().Build();
+            var helper = new CustomTabActivityHelper();
+            helper.LaunchUrlWithCustomTabsOrFallback(activity, customTabsIntent, Xamarin.Essentials.AppInfo.PackageName, Uri.Parse(url), null);
+            Device.OpenUri(new System.Uri(url));
+        }
+
+    }
+}
---
+++ xamarinJKH/InterfacesIntegration/IChromeTabs.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarinJKH.InterfacesIntegration
+{
+    public interface IChromeTabs
+    {
+        void OpenTab(string url);
+    }
+}
