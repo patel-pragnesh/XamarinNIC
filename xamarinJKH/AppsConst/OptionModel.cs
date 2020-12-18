28ff7ed6d5f53c945581e6fb25c7397ff5c2cdc1
Anton Motin <anton.motin.dev@gmail.com>
2020-12-18 10:40:01 +0300

    Новые поля в модели для иконки

--- xamarinJKH/AppsConst/OptionModel.cs
+++ xamarinJKH/AppsConst/OptionModel.cs
@@ -5,11 +5,21 @@ using Xamarin.Forms;
 
 namespace xamarinJKH.AppsConst
 {
-    public class OptionModel
+    public class OptionModel:xamarinJKH.ViewModels.BaseViewModel
     {
         public string Name { get; set; }
         public string Image { get; set; }
         public bool IsVisible { get; set; }
         public Command Command { get; set; }
+        Dictionary<string, string> replace;
+        public Dictionary<string, string> ReplaceMap
+        {
+            get => replace;
+            set
+            {
+                replace = value;
+                OnPropertyChanged("ReplaceMap");
+            }
+        }
     }
 }
