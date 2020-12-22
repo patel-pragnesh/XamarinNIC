c61e40fb198a08fb82789951c819e9d8966cbbfe
evgen <koles.doc96.09.27@gmail.com>
2020-12-22 13:50:08 +0700

    - https://tracker.yandex.ru/PROGMOBAPP-154

--- xamarinJKH/Counters/AddMetersPage.xaml.cs
+++ xamarinJKH/Counters/AddMetersPage.xaml.cs
@@ -386,9 +386,9 @@ namespace xamarinJKH.Counters
             Data.Text = String.Format(format, currentCount);
             await Task.Delay(TimeSpan.FromSeconds(2)); 
             
-             Device.BeginInvokeOnMainThread(() => {
-                 
-                 Data.Focus(); });
+             // Device.BeginInvokeOnMainThread(() => {
+             //     
+             //     Data.Focus(); });
         }
         //private void Entry_Unfocused(object sender, FocusEventArgs e)
         //{
@@ -658,7 +658,7 @@ namespace xamarinJKH.Counters
             //    d1.Unfocus();
             //    d1.Focus();
             //});
-            Device.BeginInvokeOnMainThread(() => { Data.Focus(); });
+            // Device.BeginInvokeOnMainThread(() => { Data.Focus(); });
         }
 
         string value1 = "";
@@ -750,8 +750,8 @@ namespace xamarinJKH.Counters
                                     SetCurrentValue(Convert.ToDecimal(meter.Values[0].ValueT2));
                                 //d1.Unfocus();
                                 //d1.Focus();
-                                Data.Unfocus();
-                                Data.Focus();
+                                // Data.Unfocus();
+                                // Data.Focus();
                             }
 
                             //d1.Text = "";
@@ -837,8 +837,8 @@ namespace xamarinJKH.Counters
                                     SetCurrent(Convert.ToDecimal(meter.Values[0].ValueT3));
                                 //d1.Unfocus();
                                 //d1.Focus();
-                                Data.Unfocus();
-                                Data.Focus();
+                                // Data.Unfocus();
+                                // Data.Focus();
                             }
 
                             //d1.Text = "";
